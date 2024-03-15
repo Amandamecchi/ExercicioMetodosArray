76a3f7160870255010fae6f89503f7ca808a0842
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-03-15 17:29:22 -0300

    Primeiro método: join

---
+++ AtividadeArray.js
@@ -0,0 +1,13 @@
+//Métodos Array - Aula 14-03-2024
+
+let paises = ['Romenia', 'Usa', 'Brazil'];
+
+//Método .join()
+//O método join() é usado para criar uma string a partir dos elementos de um array.
+
+//Testes:
+let res = paises.join(' ');
+console.log(res)
+
+let res2 = paises.join('-');
+console.log(res2)
